8513e6efce4819067130dc4bef6b864bfbbe9d7f
Venkata Vivek <venkatavivek@Venkatas-MacBook-Air.local>
2026-09-04 11:46:08 +0530

    chore: remove chunky scratch scripts from git tracking and update .gitignore

--- getPublications.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-const contentRaw = fs.readFileSync('./src/components/research-innovation/staticData.ts', 'utf8');
-const match = contentRaw.match(/"research-publications":\s*\{\s*"title":\s*".*?",\s*"content":\s*"(.*?)"\s*,\s*"images"/s);
-
-if (match) {
-  console.log(match[1].substring(0, 1000));
-}
